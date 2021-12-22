5cd92a3cdad8aeb0488e0878d5e7eb6fde0b8ebd
Birb <birb@gamer>
2021-12-22 18:20:50 +0100

    Docker + redeployment

---
+++ dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.9
+
+RUN apt-get update -y
+RUN apt-get install mariadb-client python3 python3-pip libmariadb-dev -y
+
+COPY ./requirements.txt /tmp/requirements.txt
+RUN pip3 install --upgrade -r /tmp/requirements.txt
+RUN pip3 install -U git+https://github.com/Pycord-Development/pycord
+
+COPY . /noahbot
+WORKDIR /noahbot
+
+ENTRYPOINT ["python3", "-u", "src/noahbot.py"]
---
+++ redeploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+git pull
+echo
+
+while true; do
+    read -p "Do you wish to redeploy NoahBot with these git changes? This will replace the image noahbot:latest [Y/n]: " yn
+    case $yn in
+        [Yy]* ) echo Deploying...; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer Y or n.";;
+    esac
+done
+
+echo
+
+CONTAINER=$(docker ps -a | grep noahbot | cut -d ' ' -f 1)
+IMAGE=$(docker images | grep 'noahbot.*latest' | awk '{print $3}')
+
+docker stop $CONTAINER && docker rm $CONTAINER
+docker image rm $IMAGE
+
+docker-compose build && docker-compose up -d && docker ps -a
+
+CONTAINER=$(docker ps -a | grep noahbot | cut -d ' ' -f 1)
+docker logs -f $CONTAINER
