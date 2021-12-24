adcd3f1e0b0daac7b818067849cbd9d3f1b2a2e9
Birb <birb@gamer>
2021-12-24 21:46:58 +0100

    Skip adding unban tasks if unban is 1+ year into the future

--- src/automation/scheduled_tasks.py
+++ src/automation/scheduled_tasks.py
@@ -17,6 +17,7 @@ async def all_tasks():
 
 async def auto_unban():
     unban_tasks = []
+    now = datetime.now()
     with connect(host=MYSQL_URI, database=MYSQL_DATABASE, user=MYSQL_USER, password=MYSQL_PASS) as connection:
         with connection.cursor() as cursor:
             query_str = """SELECT user_id, unban_time FROM ban_record WHERE unbanned = 0"""
@@ -25,8 +26,14 @@ async def auto_unban():
             for row in cursor.fetchall():
                 # row = id, user_id, unban_time
                 run_at = datetime.fromtimestamp(row[1])
+                run_at_str = run_at.strftime("%Y-%m-%d %H:%m:%S")
+
+                if (run_at - now).days > 365:
+                    STDOUT_LOG.debug(f'Skipping scheduled unban for user_id {row[0]}: is over one years into the future ({run_at_str})')
+                    continue
+
                 unban_tasks.append(schedule(unban_user(row[0]), at=run_at))
-                STDOUT_LOG.debug(f'Scheduled unban task for user_id {row[0]} at {run_at.strftime("%Y-%m-%d %H:%m:%S")}.')
+                STDOUT_LOG.debug(f'Scheduled unban task for user_id {row[0]} at {run_at_str}.')
 
     await asyncio.gather(*unban_tasks)
 
