12ce38633ebe3d9b43bbec0fac3099fe5551b21f
Birb <Birb@localnest.xyz>
2021-12-24 05:26:37 +0100

    Update IDs to HTB server

--- src/conf.py
+++ src/conf.py
@@ -4,12 +4,8 @@ from pathlib import Path
 
 from discord.commands import commands
 
-# TODO: Update this page before go-live
-
-GUILD_ID = 368879044999118848
-
+GUILD_ID = os.getenv('GUILD_ID', None)
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
-
 ROOT_DIR = Path(dirname(dirname(abspath(__file__))))
 
 MYSQL_URI = os.getenv('MYSQL_URI', 'localhost')
@@ -23,16 +19,16 @@ API_URL = f'{HTB_URL}/api'
 API_V4_URL = f'{API_URL}/v4'
 
 JOINABLE_ROLES = {
-    'Noah Gang': 915400469164220417,
-    'Buddy Gang': 915400523035852800,
-    'Red Team': 915400555009028128,
-    'Blue Team': 915402415971389451
+    'Noah Gang': 754405726432133261,
+    'Buddy Gang': 856506360999837737,
+    'Red Team': 703614597009113118,
+    'Blue Team': 703614467971350578
 }
 
 
 class ChannelIDs:
-    SR_MODERATOR = 407195797269118977
-    BOT_LOGS = 407195797269118977
+    SR_MODERATOR = 732712216641667082
+    BOT_LOGS = 756974286081622136
 
 
 def _allow(role_id):
@@ -92,14 +88,10 @@ class RoleIDs:
             'Box Creator': RoleIDs.BOX_CREATOR
         }.get(what)
 
-    # TODO: Remove these two
-    POLLOS_HERMANOS = 407196347486306314
-    SEMI_POWERS = 411892864537329685
-
     # Role collections
-    ALL_ADMINS = [ADMINISTRATOR, COMMUNITY_MANAGER, POLLOS_HERMANOS]
-    ALL_SR_MODERATOR = []
-    ALL_MODS = [MODERATOR, SEMI_POWERS]
+    ALL_ADMINS = [ADMINISTRATOR, COMMUNITY_MANAGER]
+    ALL_SR_MODERATOR = [SR_MODERATOR]
+    ALL_MODS = [SR_MODERATOR, MODERATOR]
     ALL_RANKS = [OMNISCIENT, GURU, ELITE_HACKER, PRO_HACKER, HACKER, SCRIPT_KIDDIE, NOOB, VIP, VIP_PLUS]
     ALL_CREATORS = [BOX_CREATOR, CHALLENGE_CREATOR]
     ALL_POSITIONS = [ONE, FIVE, TEN, TWENTY_FIVE, FIFTY, HUNDRED]
@@ -107,9 +99,10 @@ class RoleIDs:
 
 class SlashPerms:
     """ IDs for the specific roles. Note that due to the way slash commands handle permissions, these are SINGULAR. """
-    ADMIN = _allow(RoleIDs.POLLOS_HERMANOS)
+    COMMUNITY_MANAGER = _allow(RoleIDs.COMMUNITY_MANAGER)
+    ADMIN = _allow(RoleIDs.ADMINISTRATOR)
     SR_MODERATOR = _allow(RoleIDs.SR_MODERATOR)
-    MODERATOR = _allow(RoleIDs.SEMI_POWERS)
+    MODERATOR = _allow(RoleIDs.MODERATOR)
 
 
 class PrefixPerms:
