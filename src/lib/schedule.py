69f7d1106f1659d7a98766bbb3ad90eb74fc016d
Birb <birb@gamer>
2021-12-27 13:04:16 +0100

    Debug log in schedule.py

--- src/lib/schedule.py
+++ src/lib/schedule.py
@@ -12,13 +12,12 @@ async def schedule(task, run_at: datetime):
     await schedule(foo(1, 2), at=(dt.datetime.now() + dt.timedelta(seconds=421337)))
     """
     now = datetime.now()
-    STDOUT_LOG.debug('Started scheduling...', target_exec=run_at, current_time=now)
     if run_at < now:
-        STDOUT_LOG.debug(f'Target execution is in the past. Setting sleep timer to 0.')
+        STDOUT_LOG.debug(f'Target execution is in the past. Setting sleep timer to 0.', target_exec=run_at, current_time=now)
         seconds = 0
     else:
         seconds = int((run_at - now).total_seconds())
-        STDOUT_LOG.debug(f'Task will run after a {seconds} seconds long sleep.')
+        STDOUT_LOG.debug(f'Task will run after a {seconds} seconds long sleep.', target_exec=run_at, current_time=now)
 
     await asyncio.sleep(seconds)
     return await task
